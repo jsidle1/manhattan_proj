8e9f6ef345213a66654078d865fe16adb9df21e6
DrainCena <josephkan3@gmail.com>
2025-12-02 17:17:01 -0500

    feat: adds alerting to az5

--- az5.lua
+++ az5.lua
@@ -2,9 +2,15 @@
 
 component = require("component")
 shell = require("shell")
+local computer = require('computer')
 
 reactor_chamber_signal = component.proxy(component.get("56ec984d-8790-41f2-8305-d1014789c889"))
 
 for side = 0, 5 do
   reactor_chamber_signal.setOutput(side, 0)
 end
+
+print("AZ5 EMERGENCY SHUTDOWN INITIATED")
+for i = 1, 5 do computer.beep(1000, 0.6) end
+computer.beep(1300, 2)
+
